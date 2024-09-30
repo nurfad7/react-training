fa5a4198e952135b4ef28969a7b5642c22ffa4e0
nurfad7 <nurfad7@gmail.com>
2024-09-30 12:37:06 +0700

    fix: overlay menu button still on about when clicked

--- src/components/Hero/OverlayMenu/index.tsx
+++ src/components/Hero/OverlayMenu/index.tsx
@@ -6,11 +6,17 @@ interface OverlayMenuProps {
 }
 
 const OverlayMenu: FC<OverlayMenuProps> = ({isVisible, toggleVisibility}) => {
+    const goToAbove = () => {
+        toggleVisibility();
+        window.location.hash = '#about';
+        window.location.hash = '';
+    }
+
     return (
         <div className={`bg-black flex gap-5 justify-center items-center absolute top-1/2 right-[-18rem] 
                         h-20 w-96 text-5xl font-medium rounded-l-full hover:right-0 cursor-pointer
                         ${!isVisible ? 'block' : 'hidden'}`}
-            onClick={toggleVisibility}>
+            onClick={goToAbove}>
             <span>👋</span>
             <span className='text-white'>Hi I'm Ayush</span>
         </div>
